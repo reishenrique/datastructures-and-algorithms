8260469cf5fa814dc98f7aa62fb379f951362b7d
Henrique Reis <contatohenriquereis@gmail.com>
2022-09-20 17:35:40 -0300

    Stack structure using object instead of array

--- data-structures/stacks-usingobject.ts
+++ data-structures/stacks-usingobject.ts
@@ -1,8 +1,8 @@
 class StackObject {
-  items: number[];
+  items: {};
   count: number;
   constructor() {
-    this.items = [];
+    this.items = {};
     this.count = 0;
   }
 
@@ -10,8 +10,56 @@ class StackObject {
     this.items[this.count] = element;
     this.count++;
   }
-}
 
-const stackObject = new StackObject();
-stackObject.push(5);
-stackObject.push(10);
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return `Essa pilha tem ${this.count === 0} itens`;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.count - 1];
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+  }
+
+  cleningWholeStack() {
+    while (!this.isEmpty()) {
+      this.pop();
+    }
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      // If the stack is empty, an empty string will be delivered
+      return "";
+    }
+
+    let objString = `${this.items[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+
+    return objString;
+  }
+}
