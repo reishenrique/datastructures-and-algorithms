c52cc7476eebc9f84de8842aaa5d4ba8e683484f
Henrique Reis <contatohenriquereis@gmail.com>
2022-09-21 21:38:40 -0300

    [Problem] Converting decimal number to binary using stack structure

---
+++ data-structures/stacks/algorithm/decimal-to-binary.ts
@@ -0,0 +1,89 @@
+// Converting decimal number to binary using stack structure
+
+class StackBinaryToDemical {
+  private items: {};
+  private count: number;
+  constructor() {
+    this.items = {};
+    this.count = 0;
+  }
+
+  push(element: number) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.count === 0;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    this.count--;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+
+    return this.items[this.count - 1];
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+  }
+
+  cleningWholeStack() {
+    while (!this.isEmpty()) {
+      this.pop();
+    }
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return "";
+    }
+
+    let objString = `${this.items[0]}`;
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+
+    return objString;
+  }
+}
+
+function decimalToBinary(decNumber: number) {
+  const remStack = new StackBinaryToDemical();
+  let number = decNumber;
+  let rem: number;
+  let binaryString: string = "";
+
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+
+  while (!remStack.isEmpty()) {
+    binaryString += remStack.pop()?.toString();
+  }
+
+  return binaryString;
+}
+
+console.log(decimalToBinary(233)); // 11101001
+console.log(decimalToBinary(10)); // 1010
+console.log(decimalToBinary(1000)); // 1111101000
