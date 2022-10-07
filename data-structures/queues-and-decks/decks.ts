10112c7730d5719a7b7404e80c8129650a042b2e
Henrique Reis <contatohenriquereis@gmail.com>
2022-10-07 15:04:01 -0300

    Double-ended queue

--- data-structures/queues-and-decks/decks.ts
+++ data-structures/queues-and-decks/decks.ts
@@ -1,34 +1,17 @@
 // Deck or double-ended queue (Deque ou fila de duas pontas)
 
 class Deck {
-  count: number;
-  lowestCount: number;
-  items: {};
+  itens: any[];
   constructor() {
-    this.count = 0;
-    this.lowestCount = 0;
-    this.items;
+    this.itens;
   }
 
-  addFront(element: number | string) {
-    if (this.isEmpty()) {
-      this.addBack(element);
-    } else if (this.lowestCount > 0) {
-      this.lowestCount--;
-      this.items[this.lowestCount] = element;
-    } else {
-      for (let i = this.count; i > 0; i--) {
-        this.items[i] = this.items[i - 1];
-      }
-      this.count++;
-      this.lowestCount = 0;
-      this.items[0] = element;
-    }
+  addFront(item: number | string) {
+    return this.itens.unshift(item);
   }
 
-  addBack(element: number | string) {
-    this.items[this.count] = element;
-    this.count++;
+  addBack(item: number | string) {
+    return this.itens.push(item);
   }
 
   removeFront() {
@@ -36,10 +19,7 @@ class Deck {
       return undefined;
     }
 
-    const result = this.items[this.lowestCount];
-    delete this.items[this.lowestCount];
-    this.lowestCount++;
-    return result;
+    return this.itens.shift();
   }
 
   removeBack() {
@@ -47,38 +27,26 @@ class Deck {
       return undefined;
     }
 
-    this.count--;
-    const result = this.items[this.count];
-    delete this.items[this.count];
-    return result;
+    return this.itens.pop();
   }
 
   peekFront() {
-    if (this.isEmpty()) {
-      return undefined;
-    }
-    return this.items[this.lowestCount];
+    return this.itens[0];
   }
 
   peekBack() {
-    if (this.isEmpty()) {
-      return undefined;
-    }
-
-    return this.items[this.count - 1];
+    return this.itens[this.itens.length - 1];
   }
 
   size() {
-    return this.count - this.lowestCount;
+    return this.itens.length;
   }
 
   isEmpty() {
-    return this.size() === 0;
+    return this.itens.length === 0;
   }
 
   clear() {
-    this.count = 0;
-    this.lowestCount = 0;
-    this.items = {};
+    this.itens = [];
   }
 }
